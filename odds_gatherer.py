47d4bb78e5bc9168651ce845be2407e50f8f828a
Gyula Halmos <gyula.halmos@ericsson.com>
2018-05-01 02:00:29 +0200

    Adapt functionality to the new needs
    Only collect specific bookie data. To achieve that
    refactor the existing code.

--- odds_gatherer.py
+++ odds_gatherer.py
@@ -12,12 +12,14 @@ class OddsGatherer(object):
         self.url = kwargs.get("url", "dummy.com")
         self.sitename = kwargs.get("sitename", "dummysite")
         self.sportgenre = kwargs.get("sportgenre", "dummysport")
+        self.page_content = ""
 
     @abstractmethod
     def get_odds_from_site(self, **kwargs):
         pass
 
-    def get_prettified_page(self, url_to_scrape):
+    @staticmethod
+    def get_prettified_page(url_to_scrape):
         page_raw = requests.get(url_to_scrape)
         soup = BeautifulSoup(page_raw.content, 'html.parser')
         return soup.prettify()
@@ -62,7 +64,8 @@ class OddsChecker(OddsGatherer):
     def get_odds_from_site(self, **kwargs):
         best_odds = {}
         odds_rows = []
-        columns = ["Match-up", "Winner", "Best Odds", "Bookies"]
+        # TODO: Bookies should not be hardcoded, but stored in a dict or list!!
+        columns = ["Match-up", "Winner", "bet365", "WilliamHill"]
         pattern = self.pattern_dict[self.sportgenre]
         if pattern:
             odds_rows.append(columns)
@@ -71,7 +74,9 @@ class OddsChecker(OddsGatherer):
                 for match_url in matchups:
                     pattern = self.pattern_dict[self.sportgenre + "_matchup"]
                     matchup_title = re.findall(pattern, match_url)
-                    best_odds[matchup_title[0]] = self._get_best_odds_for_each_team(match_url)
+                    odds_for_match = self._get_best_odds_for_each_team(match_url)
+                    if odds_for_match is not None:
+                        best_odds[matchup_title[0]] = odds_for_match
             else:
                 sys.exit(1)
 
@@ -81,8 +86,8 @@ class OddsChecker(OddsGatherer):
                     # Add the match-up to the first column of the row
                     odds_row.append(k)
                     odds_row.append(team)
-                    odds_row.append(odds_info["best_odds_for_win"])
-                    odds_row.append(odds_info["bks"])
+                    odds_row.append(odds_info["bet365"])
+                    odds_row.append(odds_info["WilliamHill"])
                     odds_rows.append(odds_row)
             self.generate_csv(odds_rows)
         else:
@@ -90,20 +95,61 @@ class OddsChecker(OddsGatherer):
 
     def _get_best_odds_for_each_team(self, match_url):
         best_odds_dict = {}
-        pattern = r'data-best-bks=["]([a-zA-Z,0-9]*)["][ ]' \
-                  r'data-best-dig=["]([0-9.]*)["][ ].*[ ]' \
-                  r'data-bname=["]([a-zA-Z]*[ |/][a-zA-Z0-9]*)["]'
         matchup_content = self.get_prettified_page(match_url)
-        best_odds_for_each_team = re.findall(pattern, matchup_content)
-        if best_odds_for_each_team:
-            print(best_odds_for_each_team)
-            for odds_info in best_odds_for_each_team:
-                best_odds_for_team = dict()
-                best_odds_for_team["bks"] = odds_info[0]
-                best_odds_for_team["best_odds_for_win"] = odds_info[1]
-                best_odds_dict[odds_info[2]] = best_odds_for_team
-            return best_odds_dict
+        bookies_dict = self._get_bookies_dict(matchup_content)
+        try:
+            matchup_info = self._get_matchup_info(matchup_content)
+        except Exception as e:
+            print(match_url)
+            raise e
+        for winner in matchup_info:
+            winner_odds = self._get_odds_for_winner(winner, matchup_content)
+            try:
+               bet_365_odds = winner_odds[bookies_dict['B3']]
+               wh_odds = winner_odds[bookies_dict['WH']]
+               if "0" not in bet_365_odds and "0" not in wh_odds:
+                  best_odds_dict[winner] = {
+                           "bet365": bet_365_odds,
+                           "WilliamHill": wh_odds
+                  }
+            except IndexError as e:
+                print(winner)
+                print(winner_odds)
+                print(bookies_dict)
+                raise e
+        return best_odds_dict
+
+    @staticmethod
+    def _get_matchup_info(content):
+        pattern = r'data-name="([A-Z][A-Za-z0-9-]*[/| ][A-Z][A-Za-z0-9-]*)"'
+        matchup_parties = re.findall(pattern, content, re.S | re.M)
+        if matchup_parties:
+            return set(matchup_parties)
         else:
-            raise Exception("Could not find match odds at %s!" % match_url)
+            raise Exception("Could not find matchup opponents!")
 
+    @staticmethod
+    def _get_bookies_dict(content):
+        pattern = r'<td class="bookie-area no-grad bg([A-Z0-9]{2})'
+        bookies = re.findall(pattern, content, re.S | re.M)
+        if bookies:
+            bookies_dict = {bookie: bookies.index(bookie) for bookie in bookies}
+            return bookies_dict
+        else:
+            raise Exception("No bookies were found!")
 
+    @staticmethod
+    def _get_odds_for_winner(winner, content):
+        pattern = (
+               r'<tr class="diff-row evTabRow bc".*?data-bname="' + winner + r'"'
+               '.*?'
+               '</tr>'
+               )
+    
+        filtered = re.findall(pattern, content, re.S | re.M)
+        if filtered:
+            filtered_content = filtered[0]
+            pattern = r'<td class=".*?data-odig="([0-9]+\.*[0-9]*)".*?</td>'
+            odds = re.findall(pattern, filtered_content, re.S | re.M)
+            if odds:
+                return odds
